357a4d8871e8013dee716360bc9e97e9327f9f0c
Kevin Ta <116312994+kta-intel@users.noreply.github.com>
2024-10-30 11:31:57 -0400

    add setup.py, workflows, unit tests, and examples (#1)
    * adding setup, requirements, and initial openfl_contrib dir
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * deleting unnecessary files under openfl_contrib, update setup.py, add some basic tests
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * pulling in aggregation_function/core from base openfl library
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * inheriting objects directly from openfl
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * update eden_pipeline.py
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * add contrib workspace
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * add skc_compression workspace, add tests, add workflows
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * update initialize_and_certify_workspace function name
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * remove default workspace directory
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * removing all aggregation and pipeline functions that are not used as example
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * add custom weighted averaging and remove median, add associated tests and workflows
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * add unit test for skc compression, minr fix to custom weighted average test
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * fix version and update python req to match openfl base
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * update copyright
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * additional fixes
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * fix import
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    ---------
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    Signed-off-by: Sarthak Pati <patis@iu.edu>

---
+++ openfl_contrib/__version__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+"""openfl-contrib version information."""
+__version__ = '0.1.0'
---
+++ openfl_contrib/interface/aggregation_functions/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+"""Aggregation functions package."""
+
+from openfl_contrib.interface.aggregation_functions.custom_weighted_average import CustomWeightedAverage
