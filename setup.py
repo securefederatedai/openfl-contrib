59a7bc2b8b1102c43a864cea09b24c890da106f9
kta-intel <kevin.ta@intel.com>
2024-10-30 08:08:22 -0700

    fix version and update python req to match openfl base
    Signed-off-by: kta-intel <kevin.ta@intel.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openfl-contrib',
-    version='1.6',
+    version='0.1.0',
     author='The OpenFL Contributors',
     description='Repository for academic and research contributions to OpenFL',
     packages=find_packages(),
@@ -13,5 +13,5 @@ setup(
     install_requires=[
         'openfl @ git+https://github.com/securefederatedai/openfl.git@develop',
     ],
-    python_requires='>=3.8, <3.11'
+    python_requires='>=3.8, <3.12'
 )
