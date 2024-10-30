0cfa9bdba0183f8ea80824a42aa1b9af6455309f
Kevin Ta <116312994+kta-intel@users.noreply.github.com>
2024-10-30 11:31:57 -0400

    add setup.py, workflows, unit tests, and examples (#1)
    * adding setup, requirements, and initial openfl_contrib dir
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * deleting unnecessary files under openfl_contrib, update setup.py, add some basic tests
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * pulling in aggregation_function/core from base openfl library
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * inheriting objects directly from openfl
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * update eden_pipeline.py
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * add contrib workspace
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * add skc_compression workspace, add tests, add workflows
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * update initialize_and_certify_workspace function name
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * remove default workspace directory
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * removing all aggregation and pipeline functions that are not used as example
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * add custom weighted averaging and remove median, add associated tests and workflows
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * add unit test for skc compression, minr fix to custom weighted average test
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * fix version and update python req to match openfl base
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * update copyright
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * additional fixes
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    * fix import
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>
    
    ---------
    
    Signed-off-by: kta-intel <kevin.ta@intel.com>

---
+++ openfl_contrib/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+"""openfl base package."""
+from openfl_contrib.__version__ import __version__
---
+++ setup.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from setuptools import setup, find_packages
+
+setup(
+    name='openfl-contrib',
+    version='0.1.0',
+    author='The OpenFL Contributors',
+    description='Repository for academic and research contributions to OpenFL',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'openfl @ git+https://github.com/securefederatedai/openfl.git@develop',
+    ],
+    python_requires='>=3.8, <3.12'
+)
