3fccd4b68e5908f144bf13e220bfea5e5007f19d
Sarthak Pati <patis@iu.edu>
2025-02-22 02:31:25 +0000

    sync

---
+++ build/lib/openfl_contrib/interface/aggregation_functions/custom_weighted_average.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+"""Custom Federated averaging module."""
+
+import numpy as np
+from openfl.interface.aggregation_functions.core import AggregationFunction
+
+
+class CustomWeightedAverage(AggregationFunction):
+    """Weighted average aggregation."""
+
+    def call(self, local_tensors, *_) -> np.ndarray:
+        """Aggregate tensors.
+
+        Args:
+            local_tensors(list[openfl.utilities.LocalTensor]): List of local tensors to aggregate.
+            db_iterator: iterator over history of all tensors. Columns:
+                - 'tensor_name': name of the tensor.
+                    Examples for `torch.nn.Module`s: 'conv1.weight', 'fc2.bias'.
+                - 'round': 0-based number of round corresponding to this tensor.
+                - 'tags': tuple of tensor tags. Tags that can appear:
+                    - 'model' indicates that the tensor is a model parameter.
+                    - 'trained' indicates that tensor is a part of a training result.
+                        These tensors are passed to the aggregator node after local learning.
+                    - 'aggregated' indicates that tensor is a result of aggregation.
+                        These tensors are sent to collaborators for the next round.
+                    - 'delta' indicates that value is a difference between rounds
+                        for a specific tensor.
+                    also one of the tags is a collaborator name
+                    if it corresponds to a result of a local task.
+
+                - 'nparray': value of the tensor.
+            tensor_name: name of the tensor
+            fl_round: round number
+            tags: tuple of tags for this tensor
+        Returns:
+            np.ndarray: aggregated tensor
+        """
+        tensors, weights = zip(*[(x.tensor, x.weight) for x in local_tensors])
+
+        total_weight = sum(weights)
+        weighted_sum = np.sum([tensor * weight for tensor, weight in zip(tensors, weights)], axis=0)
+        return weighted_sum / total_weight
---
+++ build/lib/openfl_contrib_workspace/__init__.py
@@ -0,0 +1,3 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+"""openfl_contrib_workspace package."""
